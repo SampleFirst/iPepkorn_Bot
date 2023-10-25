9ca082bd700aceda493ea0a8cb32cc931d637ea0
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-25 20:28:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,6 +66,43 @@ class Bot(Client):
         me = await self.get_me()
         logger.info(f"{me.first_name} is_...  ♻️Restarting...")
 
+        # Add a job to send a message at 11:59 PM daily
+        await self.send_report_message()
+    
+    async def send_report_message(self):
+        while True:
+            tz = pytz.timezone('Asia/Kolkata')
+            today = date.today()
+            now = datetime.now(tz)
+            formatted_date_1 = now.strftime("%d-%B-%Y")
+            formatted_date_2 = today.strftime("%d %b")
+            time = now.strftime("%H:%M:%S %p")
+
+            total_users = await db.total_users_count()
+            total_chats = await db.total_chat_count()
+            today_users = await db.daily_users_count(today) + 1
+            today_chats = await db.daily_chats_count(today) + 1
+
+            if now.hour == 23 and now.minute == 59:
+                await self.send_message(
+                    chat_id=LOG_CHANNEL, 
+                    text=script.REPORT_TXT.format(
+                        a=formatted_date_1,
+                        b=formatted_date_2,
+                        c=time,
+                        d=total_users, 
+                        e=total_chats,
+                        f=today_users, 
+                        g=today_chats,
+                        h=temp.U_NAME
+                    )
+                )
+                # Sleep for 1 minute to avoid sending multiple messages
+                await asyncio.sleep(60)
+            else:
+                # Sleep for 1 minute and check again
+                await asyncio.sleep(60)
+     
     async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
         current = offset
         while True:
